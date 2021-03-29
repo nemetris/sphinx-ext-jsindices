71e550659ab70f6bf7f65c8de7eefdd3ddae90b1
Marius Schenzle <marius.schenzle@nemetris.com>
2021-03-29 17:21:04 +0200

    extend javascript object index
    * modules at toplevel
    * classes and namespaces at second level

--- sphinx_ext_jsindices/__init__.py
+++ sphinx_ext_jsindices/__init__.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from typing import Any, Dict, Iterator, List, Tuple
 
-from sphinx.domains import Index
+from sphinx.domains import Index, IndexEntry
 from sphinx.domains.javascript import JavaScriptDomain
 
 class JavaScriptModuleIndex(Index):
@@ -49,7 +49,7 @@ class JavaScriptClassIndex(Index):
 
 
 class JavaScriptObjectIndex(Index):
-    """A custom index that creates a JavaScript module, class and namespace matrix."""
+    """A custom index that creates a JavaScript module, class and namespace matrix (nested)."""
 
     name = "index"
     localname = "JavaScript Object Index"
@@ -63,12 +63,38 @@ class JavaScriptObjectIndex(Index):
                    for name, dispname, typ, docname, anchor, _
                    in self.domain.get_objects() if typ in object_filter }
 
+        prev_modname = ""
+        num_toplevels = 0
         for dispname, typ, docname, anchor in objects.values():
-            content[dispname[0].upper()].append(
-                (dispname, 0, docname, anchor, docname, '', typ))
+            subtype = 0
+            modname = dispname.split(".")[0]
+            entries = content.setdefault(modname[0].lower(), [])
+            if dispname != modname:
+                # it's an object of a module
+                # and even the first object - make parent a group head
+                if prev_modname == dispname and entries:
+                    last = entries[-1]
+                    entries[-1] = IndexEntry(last[0], subtype, last[2], last[3],
+                                            last[4], last[5], last[6])
+                elif not dispname.startswith(prev_modname):
+                    # object without parent in list, add dummy entry
+                    entries.append(IndexEntry(dispname, subtype, '', '', '', '', ''))
+                subtype = 2
+            else:
+                num_toplevels += 1
+                subtype = 1
 
-        content = sorted(content.items())
-        return content, True
+            entries.append(IndexEntry(dispname, subtype, docname, anchor,docname, '', typ))
+            prev_modname = modname
+
+        # apply heuristics when to collapse modindex at page load:
+        # only collapse if number of toplevel modules is larger than
+        # number of objects (same logic in python domain)
+        collapse = len(objects) - num_toplevels < num_toplevels
+
+        # sort by first letter
+        sorted_content = sorted(content.items())
+        return sorted_content, collapse
 
 
 def setup(app) -> Dict[str, Any]:
